8d09d928a7671f47e255b315170346ee3b77fa76
dxfuryman <94210130+DXFURYMAN@users.noreply.github.com>
2023-12-30 21:33:45 +0530

    intial

--- vite-project/weather.js
+++ vite-project/weather.js
@@ -3,19 +3,25 @@ import axios from "axios";
 
 export function getWeather(lat, lon, timezone){
 return axios.get(
-    "https://api.open-meteo.com/v1/forecast?latitude=26.9196&longitude=75.7878&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,apparent_temperature_max,apparent_temperature_min,precipitation_sum,precipitation_hours,wind_speed_10m_max,wind_gusts_10m_max&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime",
+    "https://api.open-meteo.com/v1/forecast",
     { params: {
-    latitude: lat,
+        latitude: lat,
         longitude: lon,
         timezone,
+        current: "temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m",
+        hourly: "temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m",
+        daily: "weather_code,temperature_2m_max,apparent_temperature_max,apparent_temperature_min,precipitation_sum,precipitation_hours,wind_speed_10m_max,wind_gusts_10m_max",
+        wind_speed_unit: "mph",
+        precipitation_unit: "inch",
+        timeformat: "unixtime"
     },
     }
-    ).then(({data}) => {
-        return {
-            current:parseCurrentWeather(data),
-            daily: parseDailyWeather(data),
-            hourly: parseHourlyWeather(data)
-        }
+).then(({data}) => {
+    return {
+        current: parseCurrentWeather(data.current),
+        daily: parseDailyWeather(data.daily),
+        hourly: parseHourlyWeather(data.hourly)
+    }
 })
 }
 function parseCurrentWeather({ apparent_temperature, daily}){
